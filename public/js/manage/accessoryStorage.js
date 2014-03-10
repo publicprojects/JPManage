d8badf387728bb4b18cd7763de04f39c6673fa7d
gechaoqing <gechaoqing00@gmail.com>
2014-03-10 22:20:55 +0800

    其他管理功能

---
+++ public/js/manage/accessoryStorage.js
@@ -0,0 +1,43 @@
+/**
+ * Created by chaoqing on 14-3-2.
+ */
+$(function(){
+    var cols=[{
+        name:"辅料名称",
+        field:"name"
+    },{
+        name:"客户",
+        field:"createAt"
+    },{
+        name:"日期",
+        field:"updateAt"
+    },{
+        name:"发生量",
+        field:"updateAt"
+    },{
+        name:"出/入库",
+        field:"updateAt"
+    },{
+        name:"操作",
+        data:[{
+            name:"<i class='icon-edit'></i> 修改",
+            href:"/manager/getDataById/1/{id}",
+            className:"blue"
+        },{
+            name:"<b>&times;</b> 删除",
+            href:"/manager/delDataConfirm/1/{id}",
+            className:"red"
+        }],
+        type:"operator"
+    }];
+    dataTable=TableJS.init({
+        titles:cols,
+        container:".dataTable-container",
+//        url:"/manager/getDataTable/1",
+        data:[],
+        urlPara:{}
+    });
+    $(".tool-bar span.btn").click(function(){
+        TableJS.modal($(this).attr("data-href"));
+    })
+});
